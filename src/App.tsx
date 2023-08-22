b90e1aa285f82c9a01789ed303eaf4678c4fd477
luis-studioG <luis@studiographene.com>
2023-08-22 14:12:54 +0100

    added change 2

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,9 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+        <p>
+          Testing workflows on Github
+        </p>
       </div>
     </>
   )
