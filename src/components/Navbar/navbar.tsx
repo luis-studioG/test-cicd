1fea0eb48d8941ebfcb2d8a66c73e41c1cc63050
luis-studioG <luis@studiographene.com>
2023-08-29 14:46:17 +0100

    added li to navbar

--- src/components/Navbar/navbar.tsx
+++ src/components/Navbar/navbar.tsx
@@ -1,16 +1,17 @@
-// import { useState } from "react";
 import reactLogo from "../../assets/react.svg";
-// import viteLogo from '/vite.svg'
 import "./navbar.css";
 
-export const Navbar = () => 
-    <nav className="nav-wrapper">
-        <div className="title-container">
-            <span>Learning CI/CD</span>
-            <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
-                <img src={reactLogo} className="logo react" alt="React logo" />
-            </a>
-        </div>
-    </nav>;
+const Navbar = () =>
+        <nav className="nav-wrapper">
+            <div className="title-container">
+                <span>Learning CI/CD</span>
+                <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
+                    <img src={reactLogo} className="logo react" alt="React logo" />
+                </a>
+            </div>
+            <ul className="list">
+                <li>CI checks at SG</li>/
+            </ul>
+        </nav>;
 
 export default Navbar;
